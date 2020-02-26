6b543c271959c4f21f642eb0afa050bec00c6587
Rubber-Duck-999 <simon.crowther95@hotmail.co.uk>
2020-02-26 09:15:09 +0000

    Formatted DBM

--- DatabaseManager/src/main/java/TopicRabbitmq.java
+++ DatabaseManager/src/main/java/TopicRabbitmq.java
@@ -1,53 +1,45 @@
 
-import java.time.format.DateTimeFormatter;  
-import java.time.LocalDateTime;   
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 
-public class TopicRabbitmq
-{    
+public class TopicRabbitmq {
     private String _routingKey;
     private String _message;
     private LocalDateTime _timeOfReceival;
     private boolean _validTopic;
-    
-    private void SetTime()
-    {
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
-        _timeOfReceival = LocalDateTime.now(); 
+
+    private void SetTime() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        _timeOfReceival = LocalDateTime.now();
     }
-    
-    public TopicRabbitmq()
-    {
+
+    public TopicRabbitmq() {
         _routingKey = "";
         _message = "";
         this.SetTime();
         _validTopic = true;
     }
 
-    public TopicRabbitmq(String routingKey, String message)
-    {
+    public TopicRabbitmq(String routingKey, String message) {
         _routingKey = routingKey;
         _message = message;
         this.SetTime();
         _validTopic = false;
     }
-    
-    public boolean GetValidity()
-    {
+
+    public boolean GetValidity() {
         return _validTopic;
     }
-    
-    public void SetInValidTopic()
-    {
+
+    public void SetInValidTopic() {
         _validTopic = false;
     }
 
-    public String GetMessage()
-    {
+    public String GetMessage() {
         return _message;
     }
 
-    public String GetRoutingKey()
-    {
+    public String GetRoutingKey() {
         return _routingKey;
     }
 }
