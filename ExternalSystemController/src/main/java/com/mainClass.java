1800ff634eb0fab7bc79a4f311f449d104350282
Simon Crowther <simon.crowther95@hotmail.co.uk>
2020-03-25 16:11:57 +0000

    Create basic ESC package

---
+++ ExternalSystemController/src/main/java/com/mainClass.java
@@ -0,0 +1,13 @@
+package com;
+
+import java.util.logging.Logger;
+import com.house_guard.Common.*;
+
+public class mainClass
+{
+
+    public static void main(String[] argv) throws Exception
+    {
+        
+    }
+}
