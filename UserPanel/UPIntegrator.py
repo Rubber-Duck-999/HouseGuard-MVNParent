16e08b8348a9fb0e9415e21972032f25d360a516
Rubber-Duck-999 <simon.crowther95@hotmail.co.uk>
2020-04-01 20:04:01 +0100

    Updated Event calls and removal of temporary password

---
+++ UserPanel/UPIntegrator.py
@@ -0,0 +1,62 @@
+'''
+Created on 10 Oct 2019
+
+@author: simon
+'''
+
+#!/usr/bin/env python
+import pika
+import sys, time, json
+
+###
+# User Panel Simulator Interface 
+# This is to show how the DBM could manager
+# its necessary pub & sub topics with rabbitmq
+
+## Setup connection and exchange
+credentials = pika.PlainCredentials('guest', 'password')
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', 5672, '/', credentials))
+channel = connection.channel()
+channel.exchange_declare(exchange='topics', exchange_type='topic', durable=True)
+#
+
+# Specific settings for subscribe key
+print("## Beginning UP Integrator")
+result = channel.queue_declare('', exclusive=False, durable=True)
+queue_name = result.method.queue
+topic_monitor = "Monitor.State"
+topic_request_access = "Request.Access"
+topic_response = "Access.Response"
+topic_event = "Event.UP"
+#channel.queue_bind(exchange='topics', queue=queue_name, routing_key=topic_monitor)
+channel.queue_bind(exchange='topics', queue=queue_name, routing_key=topic_request_access)
+print(' UP Integrator [*] Waiting for topics. To exit press CTRL+C')
+
+time.sleep(0.5)
+#channel.basic_publish(exchange='topics', routing_key=topic_data_request, body=payload)
+#print("Sent %r " % topic_data_request)
+
+
+def callback(ch, method, properties, body):
+    print(" DBM received an event [x] %r:%r" % (method.routing_key, body))
+    if method.routing_key == topic_request_access:
+        ##
+        my_json = body.decode('utf8')
+        received = json.dumps(my_json)
+        print(received)
+        print(received["id"])
+        #id = received['id']
+        ##
+        data = {
+            "id": id, 
+            "result":True  
+        }
+        #payload = json.dumps(data)
+        #print(payload)
+        #channel.basic_publish(exchange='topics', routing_key=topic_response, body=payload)
+        #print("Sent %r " % topic_response)
+
+channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+channel.start_consuming()
+connection.close()
