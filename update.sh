4cd6ed170de9103878d6a56c0b1a08b50f1b1da1
Rubber-Duck-999 <simon.crowther95@hotmail.co.uk>
2021-01-18 16:49:32 +0000

    Fix update.sh

--- update.sh
+++ update.sh
@@ -8,17 +8,6 @@ mvn clean
 
 mvn install
 
-if [ -f DatabaseManager/target/DatabaseManager* ];
-then
-    echo "DBM File found"
-    if [ -f $HOME/Documents/Deploy/DatabaseManager.jar ];
-    then
-        echo "DBM old removed"
-        rm -f $HOME/Documents/Deploy/DatabaseManager.jar
-    fi
-    mv DatabaseManager/target/DatabaseManager* $HOME/Documents/Deploy/DatabaseManager.jar
-fi
-
 if [ -f UserPanel/target/UserPanel* ];
 then
     echo "UP File found"
